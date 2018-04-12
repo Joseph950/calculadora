fdf7767e5e18f34d747cf3227c2d2374ea8867e9
Joseph <mendietacruzjose@gmail.com>
2018-04-12 14:14:06 -0500

    La calculadora realiza la operacion sumar correctamente

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -68,6 +69,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Sumar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,9 +12,23 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
+        Double c1 = 0, c2 = 0, Suma;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c1 = Double.Parse(textBox1.Text);
+            c2 = Double.Parse(textBox2.Text);
+            Suma = c1 + c2;
+            listBox1.Items.Add(Suma.ToString());
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
